a7d0805d8fda433af6e7244a2767a7aeb58240a0
Papa Yaw <pyamoako@outlook.com>
2024-04-16 00:27:55 -0400

    odd and even bash script added

--- newpy.sh
+++ newpy.sh
@@ -1,1 +1,33 @@
-This is a practice file to test my git repo.
+# This is a practice file to test my git repo.
+
+#!/bin/bash
+
+#Let's test break and continue commands
+
+for (( n=1; n<=50; n++ )) 
+do
+ 
+if [ $n = 23 -o $n = 24 ];then
+   break
+   
+fi
+
+echo "$n"
+
+done
+
+count=1
+
+while [ $count -le 22 ]; do
+
+  if [ $(( count % 2 )) -eq 0 ]; then
+       echo "EVEN"
+  else 
+       echo "ODD"
+
+  fi
+
+    ((count++))
+
+done
+    
